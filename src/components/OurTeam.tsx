8d64136149302d34f2ea47f7ee9a15b4381b27e3
sharukhk08 <iamsharukh08@gmail.com>
2021-12-19 21:23:55 +0530

    our team section init

---
+++ src/components/OurTeam.tsx
@@ -0,0 +1,27 @@
+const OurTeam = () => {
+  return (
+    <>
+      <div className="our-team-section ">
+        <div className="container">
+          <div className="row">
+            <div className="col-11 d-flex justify-content-center align-items-center flex-column  text-center">
+              <h1 className="text-center text-white fs-xxl fs-normal">
+                Roadmap
+              </h1>
+
+              <p>
+                llamadramaclub was created by a group of creatives with a
+                passion for music, theater and animation. It is our goal to
+                spearhead projects of various genres and collaborate with our
+                favorite artists who dabble in art, short film animation and
+                gaming.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurTeam;
