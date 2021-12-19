cf20be575529157208ed893ea4bcb19e7c75f40e
Radial-code <work@radialcode.com>
2021-12-19 18:53:18 +0530

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "../styles/about.css";
+import AboutImg from "../assets/aboutMint.png";
+const About = () => {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="d-flex justify-content-between about-us-layout gx-5 flex-lg-row flex-column align-items-center">
+            <div className=" mb-5">
+              <div className="aboutLeft">
+                <img src={AboutImg} alt="" className="w-100" />
+              </div>
+            </div>
+            <div className=" mb-5">
+              <div className="aboutRight">
+                <small className="text-orange-800">About Us</small>
+                <h1 className="text-white fs-xxl  text-white">
+                  Welcome to the llamadramaclub!
+                </h1>
+                <h6 className="fs-md fw-regular leading-30 text-grey-900">
+                  The first NFT project that plans to go fully mainstream with
+                  animated short films and video games. One NFT allows you
+                  access to the game world with freebies and early access to
+                  animated short films.
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+    // <div id="aboutPage">
+    //   <img src={AboutImg} className="w-100" alt="" />
+    //   <h1 id="aboutTitle">LLAMADRAMACLUB</h1>
+    //   <p className="aboutText">
+    //     was created by a group of creatives with a passion for music, theater
+    //     and animation.
+    //   </p>
+    //   <p className="aboutText">
+    //     It is our goal to spearhead projects of various genres and collaborate
+    //     with our favorite artists who dabble in art, short film animation and
+    //     gaming.
+    //   </p>
+    // </div>
+  );
+};
+
+export default About;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import "../styles/navbar.css";
+//import react responsive
+import { useMediaQuery } from "react-responsive";
+//import logos
+import twitterLogo from "../assets/MdiTwitter.svg";
+import youtubeLogo from "../assets/MdiYoutube.svg";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  //Off canbas config
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+  //react responsive config
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1000px)",
+  });
+  //enable offcanvas scrolling
+  const handleShow = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setShow(true);
+  };
+  return (
+    <>
+      <section className="bg-black-600 py-3 club-header">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light text-white ">
+            <div className="container-fluid">
+              <a className="navbar-brand logo text-white" href="#">
+                llamadramaclub
+              </a>
+              <button
+                className="navbar-toggler bg-white"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav align-items-lg-center ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active text-white"
+                      aria-current="page"
+                      href="#"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-white" href="#">
+                      About
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link text-white" href="#">
+                      Roadmap
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-white" href="#">
+                      Twitter
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-white" href="#">
+                      Youtube
+                    </a>
+                  </li>
+                  <li className=" nav-item">
+                    <a
+                      href="https://discord.gg/pZbPMA3r"
+                      target="_blank"
+                      className="nav-link ms-3 d-flex justify-content-center align-items-center text-white discord-btn"
+                    >
+                      {" "}
+                      Discord
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </section>
+      {/* <Navbar id="navbarDiv" bg="dark" expand="lg" fixed="top">
+        <Container fluid>
+          <Navbar.Brand href="#landingPage" id="navbarTitle">
+            <img id="realLogo" src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#roadmapComponent">
+                <p className="canvasLink">ROADMAP</p>
+              </Nav.Link>
+              <Nav.Link href="#aboutPage">
+                {" "}
+                <p className="canvasLink">ABOUT</p>
+              </Nav.Link>
+              <Nav.Link href="#faqComponent">
+                {" "}
+                <p className="canvasLink">FAQ</p>
+              </Nav.Link>
+              <Nav.Link
+                href="https://twitter.com/llamadramaclub"
+                target="_blank"
+              >
+                {" "}
+                <p className="canvasLink">Twitter</p>
+              </Nav.Link>{" "}
+              <Nav.Link
+                href="https://www.youtube.com/channel/UCWbjIu44mFe536Mf76jGjlg"
+                target="_blank"
+              >
+                {" "}
+                <p className="canvasLink">Youtube</p>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> */}
+    </>
+  );
+};
+
+export default NavBar;
