a76e1344a125996d5b19debe7d866b0b81051623
llamadramaclub <95920946+llamadramaclub@users.noreply.github.com>
2022-01-24 19:26:44 +0100

    Revert "cm v2"

--- src/MintApp.tsx
+++ src/MintApp.tsx
@@ -1,58 +1,72 @@
-import './App.css';
-import { useMemo } from 'react';
-import * as anchor from '@project-serum/anchor';
-import Home from './Home';
+import "./MintApp.css";
+import { useMemo } from "react";
 
-import { CrossMintProvider } from "@crossmint/client-sdk-react-ui";
-import { clusterApiUrl } from '@solana/web3.js';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import Home from "./Home";
+
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   getPhantomWallet,
   getSlopeWallet,
   getSolflareWallet,
   getSolletWallet,
   getSolletExtensionWallet,
-} from '@solana/wallet-adapter-wallets';
+} from "@solana/wallet-adapter-wallets";
 
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
-import { WalletDialogProvider } from '@solana/wallet-adapter-material-ui';
+} from "@solana/wallet-adapter-react";
 
-import { ThemeProvider, createTheme } from '@material-ui/core';
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { createTheme, ThemeProvider } from "@material-ui/core";
 
-//require("../styles/globals.css");
-require("@crossmint/client-sdk-react-ui/styles.css");
+const treasury = new anchor.web3.PublicKey(
+  process.env.REACT_APP_TREASURY_ADDRESS!
+);
+
+const config = new anchor.web3.PublicKey(
+  process.env.REACT_APP_CANDY_MACHINE_CONFIG!
+);
+
+const candyMachineId = new anchor.web3.PublicKey(
+  process.env.REACT_APP_CANDY_MACHINE_ID!
+);
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
 
 const theme = createTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
+  },
+  overrides: {
+    MuiButtonBase: {
+      root: {
+        justifyContent: "flex-start",
+      },
+    },
+    MuiButton: {
+      root: {
+        textTransform: undefined,
+        padding: "12px 16px",
+      },
+      startIcon: {
+        marginRight: 8,
+      },
+      endIcon: {
+        marginLeft: 8,
+      },
+    },
   },
 });
-
-const getCandyMachineId = (): anchor.web3.PublicKey | undefined => {
-  try {
-    const candyMachineId = new anchor.web3.PublicKey(
-      process.env.REACT_APP_CANDY_MACHINE_ID!,
-    );
-
-    return candyMachineId;
-  } catch (e) {
-    console.log('Failed to construct CandyMachineId', e);
-    return undefined;
-  }
-};
-
-const candyMachineId = getCandyMachineId();
-const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
-const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
-const connection = new anchor.web3.Connection(rpcHost
-  ? rpcHost
-  : anchor.web3.clusterApiUrl('devnet'));
-
-const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
-const txTimeoutInMilliseconds = 30000;
 
 const App = () => {
   const endpoint = useMemo(() => clusterApiUrl(network), []);
@@ -60,27 +74,27 @@ const App = () => {
   const wallets = useMemo(
     () => [
       getPhantomWallet(),
-      getSolflareWallet(),
       getSlopeWallet(),
+      getSolflareWallet(),
       getSolletWallet({ network }),
       getSolletExtensionWallet({ network }),
     ],
-    [],
+    []
   );
 
   return (
     <ThemeProvider theme={theme}>
       <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
+        <WalletProvider wallets={wallets} autoConnect={true}>
           <WalletDialogProvider>
-          <CrossMintProvider clientId="f012ecdf-55c2-4dbd-b8f4-d2b5c5f95828">
             <Home
               candyMachineId={candyMachineId}
+              config={config}
               connection={connection}
               startDate={startDateSeed}
-              txTimeout={txTimeoutInMilliseconds}
-              rpcHost={rpcHost}
-            /></CrossMintProvider>
+              treasury={treasury}
+              txTimeout={txTimeout}
+            />
           </WalletDialogProvider>
         </WalletProvider>
       </ConnectionProvider>
